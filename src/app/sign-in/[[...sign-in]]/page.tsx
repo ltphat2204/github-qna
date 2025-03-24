273bc90055a79ea515e7bbb5a42a1ea35a614798
ltphat2204 <ltphat2204@gmail.com>
2025-03-24 21:03:46 +0700

    Style sign up and sign in pageW

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return <div className="min-h-screen flex items-center justify-center">
+    <SignIn />
+  </div>
 }
